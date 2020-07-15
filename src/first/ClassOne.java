787d8762a62cacfe494daa94885245a214423463
TestUser <TestUser@mail.ru>
2020-07-15 21:45:26 +0300

    добавление первого коммита

---
+++ src/first/ClassOne.java
@@ -0,0 +1,13 @@
+package first;
+
+public class ClassOne {
+
+    public static void main(String[] args) {
+        
+    }
+
+    private static int numberPlus(int number_1, int number_2){
+        return number_1 + number_2;
+    }
+
+}
