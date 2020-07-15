af7130c69b345f0248a39e78e23c628bb49dfbc8
TestUser <TestUser@mail.ru>
2020-07-15 22:25:57 +0300

    второй коммит. сложение чисел

--- src/first/ClassOne.java
+++ src/first/ClassOne.java
@@ -1,13 +1,36 @@
 package first;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
 public class ClassOne {
 
     public static void main(String[] args) {
-        
+
+        Scanner in = new Scanner(System.in);
+        int one = in.nextInt();
+        int two = in.nextInt();
+
+        System.out.println(numberPlus(one, two));
     }
 
+    //сложение функций
     private static int numberPlus(int number_1, int number_2){
         return number_1 + number_2;
+    }
+
+    //сравнивание чисел
+    private static int compareNumbers(int number_1, int number_2) throws Exception {
+        if(number_1 < number_2){
+            return number_2;
+        }
+        else if (number_1 > number_2){
+            return number_1;
+        }
+        else {
+            throw new Exception("введенные числа равны");
+        }
     }
 
 }
