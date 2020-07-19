c687ad776e326fe8a1f67fe214a840bb398c7f59
TestUser <TestUser@mail.ru>
2020-07-19 13:12:35 +0300

    добавлено исключение на введеное не корректное число

--- src/first/ClassOne.java
+++ src/first/ClassOne.java
@@ -2,6 +2,7 @@ package first;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ClassOne {
@@ -9,11 +10,22 @@ public class ClassOne {
     public static void main(String[] args) throws Exception{
 
         Scanner in = new Scanner(System.in);
-        int one = in.nextInt();
-        int two = in.nextInt();
+        int one;
+        int two;
+
+        try {
+            one = in.nextInt();
+            two = in.nextInt();
+        }
+        catch (InputMismatchException e){
+            throw new InputMismatchException("Ввели не целое число!!!");
+        }
+        finally {
+            in.close();
+        }
 
         System.out.println(numberPlus(one, two));
-        System.out.println(compareNumbers(one, two));
+
     }
 
     //сложение функций
